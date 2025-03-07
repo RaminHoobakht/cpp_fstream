36ea3ca3e0e5300cc1694d5da4fd4ceb35e1cccf
hinata <hinatachikao1971@gmail.com>
2025-03-07 06:57:50 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,48 +1,11 @@
-#include "../header/main.hpp"
-#include <fstream>
-#include <ios>
 #include <iostream>
+#include "../header/main.hpp"
+
 
 int main() {
 
-    constexpr size_t buff_size{1024LU};
-    char read_buffer[buff_size]{};
 
-    constexpr auto file_name{"temp_test.txt"};
-    constexpr auto in_mode{std::ios::in};
-    constexpr auto out_mode{std::ios::out};
-    constexpr auto app_mode{std::ios::app};
 
-    std::fstream fs{};
-
-    fs.open(file_name, in_mode | out_mode | app_mode);
-    if (!fs) {
-        perror("error in open file");
-        exit(EXIT_FAILURE);
-    }
-
-    fs << "I am a C/C++ Developer" << NL;
-
-    if (fs.is_open()) {
-        fs.close();
-    }
-
-    fs.open(file_name, in_mode | out_mode | app_mode);
-    if (!fs) {
-        perror("error in open file");
-        exit(EXIT_FAILURE);
-    }
-
-    fs.read(read_buffer, buff_size - 1);
-
-    if (fs.is_open()) {
-        fs.close();
-    }
-
-    std::cout << "countent of file is: " << NL;
-    std::cout << read_buffer << NL;
-    SEP;
-
-    std::cout << "\n #(23:40:04): The End ..." << std::endl;
+    std::cout << "\n #(06:57:50): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
